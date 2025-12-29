c642ef0be57b633afaa09dddda257af0e0057999
Josiasxz <josuemt112@gmail.com>
2025-12-29 20:54:51 -0300

    Multiplicação e divisão
    Adicionei multiplicação e divisão a calculadora basica. Tambem coloquei alguns comentarios para me orientar melhor

--- Programas/calc.c
+++ Programas/calc.c
@@ -1,31 +1,50 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-   float v1 = get_float ("Digite um valor: ");
-   float v2 = get_float ("Digite outro valor: ");
-   float result = 0;
+    // Variaveis principais, como valores e o resultado
+    float v1 = get_float("Digite um valor: ");
+    float v2 = get_float("Digite outro valor: ");
+    float result = 0;
 
-    // Pede para selecionar a operação
-   printf("Escolha algum dos valores para determinar sua operação\n");
-   printf("1 - Multiplicar\n2 - Subtrair\n");
+    // Pede para selecionar a operação desejada
+    printf("Escolha algum dos valores para determinar sua operação\n");
+    printf("1 - Multiplicar\n2 - Subtrair\n3 - Multiplicar\n4 - Dividir\n");
 
-   int op = get_int ("Selecione sua operação: ");
+    int op = get_int("Selecione sua operação: ");
 
-   while (op > 2 || op < 1) {
-      op = get_int ("Selecione uma operação valida: ");
-   }
-
-   if (op == 1){
-      result = v1 + v2;
-      printf("O resultado da multiplicação é %f\n", result);
-   } else {
-      result = v1 - v2;
-      printf("O resultado da subtração é %f\n", result);
-   }
-
-
-   return 0;
-
+    //Cria um loop que verifica a operação realizada até que ela seja verdadeira
+    while (op > 4 || op < 1)
+    {
+        op = get_int("Selecione uma operação valida: ");
+    }
+    //Verifica qual operação escolhida e faz o calculo com base na operação escolhida
+    // Adição
+    if (op == 1)
+    {
+        result = v1 + v2;
+        printf("O resultado da multiplicação é %f\n", result);
+    }
+    //Subtração
+    else if (op == 2)
+    {
+        result = v1 - v2;
+        printf("O resultado da subtração é %f\n", result);
+    }
+    //Multiplicação
+    else if (op == 3)
+    {
+        result = v1 * v2;
+        printf("O resultado da multiplicação é %f\n", result);
+    }
+    //Divisão
+    else
+    {
+        result = v1 / v2;
+        printf("O resultado da divisão é %f\n", result);
+    }
+    //Finaliza o codigo
+    return 0;
 }
+
